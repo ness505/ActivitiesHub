e1dee92db055b476a37b2961799d950c08e5b8c4
Vanessa Aguayo <vanessa.aguayo.777@gmail.com>
2026-08-25 18:39:29 -0600

    add entity and orm boilerplate

---
+++ src/Domain/Activity.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain;
+
+public class Activity
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Title { get; set; }
+    public DateTime Date { get; set; }
+    public required string Description { get; set; }
+    public required string Category { get; set; }
+    public bool IsCancelled { get; set; }
+    public required string City { get; set; }
+    public required string Venue { get; set; }
+    public required string Latitude { get; set; }
+    public required string Longitude { get; set; }
+}
