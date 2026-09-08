8c1d739424cf1105cf918d5bc81cb258d0d01a8b
Vanessa Aguayo <84104194+ness505@users.noreply.github.com>
2026-09-07 18:29:24 -0600

    Revert "Parcial01"

--- src/EventsHub.Api/Controllers/EventsHubBaseController.cs
+++
@@ -1,8 +0,0 @@
-
-using Microsoft.AspNetCore.Mvc;
-
-namespace EventsHub.Api.Controllers;
-
-[Route("api/v1/[controller]")]
-[ApiController]
-public class EventsHubBaseController : ControllerBase { }
