4071ecb223d9bd99577a3b0ccba7d86eb6d3f3ab
Vanessa Aguayo <vanessa.aguayo.777@gmail.com>
2026-09-12 12:00:13 -0600

    add CORS

--- src/EventsHub.Api/Program.cs
+++ src/EventsHub.Api/Program.cs
@@ -10,7 +10,14 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(opt => opt
+.AllowAnyHeader()
+.AllowAnyMethod()
+.WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) { }
--- web/EventsHub/src/App.tsx
+++ web/EventsHub/src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react"
+
 function App() {
 
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/v1/events')
+      .then(response => response.json())
+      .then(data => setActivities(data));
+
+    return () => { };
+  }, []);
+
   return (
-    <h3>EventsHub</h3>
+    <>
+      <h3 style={{ color: 'red' }}>EventsHub</h3>
+      <ul>
+        {activities?.map((act) => (
+          <li key={act.id}>{act.title}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
